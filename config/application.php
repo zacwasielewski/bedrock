2ce048a0ce38bcb5d44011c1a32d8daa03582e9e
Zac Wasielewski <zac@wasielewski.org>
2014-05-08 21:01:56 +0000

    Run  from bedrock-ansible, then modify config for MultiSite

--- config/application.php
+++ config/application.php
@@ -70,3 +70,14 @@ define('DISALLOW_FILE_EDIT', true);
 if (!defined('ABSPATH')) {
   define('ABSPATH', $webroot_dir . '/wp/');
 }
+
+/** MultiSite
+ *
+ */
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+$base = '/';
+define('DOMAIN_CURRENT_SITE', getenv('DOMAIN_CURRENT_SITE'));
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
