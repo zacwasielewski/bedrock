ddc4daca436dd137d342ea2971ce2066f3591625
Zac Wasielewski <zac@wasielewski.org>
2014-08-10 23:21:27 +0000

    Install inlet-wedding v0.1.1 theme

---
+++ web/app/themes/inlet-wedding/base.php
@@ -0,0 +1,26 @@
+<?php get_template_part('templates/head'); ?>
+<body <?php body_class(); ?>>
+
+  <!--[if lt IE 8]>
+    <div class="alert alert-warning">
+      <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'roots'); ?>
+    </div>
+  <![endif]-->
+
+  <div class="wrap" role="document">
+    <div class="content">
+      <main class="main <?php echo roots_main_class(); ?>" role="main">
+        <?php include roots_template_path(); ?>
+      </main><!-- /.main -->
+      <?php if (roots_display_sidebar()) : ?>
+        <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
+          <?php include roots_sidebar_path(); ?>
+        </aside><!-- /.sidebar -->
+      <?php endif; ?>
+    </div><!-- /.content -->
+  </div><!-- /.wrap -->
+
+  <?php get_template_part('templates/footer'); ?>
+
+</body>
+</html>
