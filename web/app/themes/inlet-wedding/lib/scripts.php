ba7b030676e4813aab0a0d489a45af3b8154521c
Zac Wasielewski <zac@wasielewski.org>
2014-08-14 00:50:37 +0000

    Update inlet-wedding theme to v0.1.4

--- web/app/themes/inlet-wedding/lib/scripts.php
+++ web/app/themes/inlet-wedding/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9a374d71dbd0544105d8870e0f4b0f7c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '34a86dd487558eaf7d5c7b5fe1a6e20f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
